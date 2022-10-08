a3b4f4ce45d10f3ce57a0ed28a893b4068f6e342
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-10-07 21:02:48 -0500

    Add files via upload
    Actualización del curso de JavaScript

---
+++ Leccion23/js/app.js
@@ -0,0 +1,36 @@
+
+const personas = [
+    new Persona("Victor","Lopez"),
+    new Persona("Liam","Vasquez")
+];
+
+function mostrarPersonas(){
+    console.log("Mostrar Personas");
+    let texto = "";
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById("personas").innerHTML=texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms["forma"];
+    const nombre = forma["nombre"];
+    const apellido = forma["apellido"];
+
+    if(nombre.value != "" && apellido.value != ""){
+       
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+    
+        mostrarPersonas();
+    }
+    else{
+        console.log("Cadena de texto vacio");
+    }
+
+ 
+}
+
